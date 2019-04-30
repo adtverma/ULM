ad09158c719ce7b57ef4a9520d2b7e1aeefddfdf
Rahul <rahul@jaisimha.com>
2019-04-30 00:13:26 -0400

    Add overlay and ugly text

--- js/index.js
+++ js/index.js
@@ -1,4 +1,7 @@
 function expand(circle) {
+    showOverlay();
+    var blurb = circle.getElementsByClassName("textBlurb");
+    blurb[0].style.visibility = "visible";
     /*contractAll();
     circle.classList.add("expanded");
     console.log(circle.value);*/
@@ -8,5 +11,24 @@ function contractAll() {
     var expanded = document.getElementsByClassName("expanded");
     for (let el of expanded) {
         el.classList.remove("expanded");
+    }
+}
+
+function showOverlay() {
+    var overlay = document.getElementById("overlay");
+    var bigCircle = document.getElementById("bigCircle");
+    overlay.classList.add("fadeIn");
+    overlay.style.visibility = "visible";
+    bigCircle.style.visibility = "visible";
+}
+
+function hideOverlay() {
+    var overlay = document.getElementById("overlay");
+    var bigCircle = document.getElementById("bigCircle");
+    overlay.classList.remove("fadeIn");
+    overlay.style.visibility = "hidden";
+    bigCircle.style.visibility = "hidden";
+    for (let blurb of document.getElementsByClassName("textBlurb")) {
+        blurb.style.visibility = "hidden";
     }
 }
