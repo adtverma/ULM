5c4fcdb6c78123e0840a934a3b043a463f86698e
adtverma <49669027+adtverma@users.noreply.github.com>
2019-05-06 03:03:54 -0400

    small edit

--- js/index.js
+++ js/index.js
@@ -44,7 +44,7 @@ function showBlurb(i) {
             break;
         case 1:
             header = "Ideology";
-            txt = "The short existence of the school had a lasting impact on design education through its use of experimental pedagogy and through creating overt relations with industry. HfG Ulm's experimental approach to teaching included the abolishment of departments as traditionally conceived within a university setting; instead disciplines were grouped around topics such as, Industrial Design, Visual Communication, Building, Information, and Film. The system of a large proportion of visiting faculty, created an atmosphere of constant re-evaluation and critique, gaining HfG Ulm its reputation as a centre for cutting edge research and teaching in design. Its emphasis on the social responsibility of the designer also helped in the re-orientation of design as an inherently political activity. This platform seeks to build on these ideals to develop a contemporary course structure targeted towards fostering designers and thinkers.";
+            txt = "The short existence of ULM had a lasting impact on design education through its use of experimental pedagogy and through creating overt relations with industry. HfG Ulm's experimental approach to teaching included the abolishment of departments as traditionally conceived within a university setting; instead disciplines were grouped around topics such as, Industrial Design, Visual Communication, Building, Information, and Film. The system of a large proportion of visiting faculty, created an atmosphere of constant re-evaluation and critique, gaining HfG Ulm its reputation as a centre for cutting edge research and teaching in design. Its emphasis on the social responsibility of the designer also helped in the re-orientation of design as an inherently political activity. This platform seeks to build on these ideals to develop a contemporary course structure targeted towards fostering designers and thinkers.";
             break;
         case 2:
             header = "Courses";
@@ -67,9 +67,9 @@ function showBlurb(i) {
         case 7:
             header = "Workshops";
             txt = "Join us for a demonstrative session for our courses in New York City! </br> \
-                1. Economics of Misinformation- March 18th, 2019 (11:30-2pm) Tony Hall (BBC Finance Reporter) and Giorgia Lupi (Data Visualiser and Design Researcher)</br> \
-                2. Anonimity and Gender Erasure- May 20th, 2019 (6-8pm) Durga Chew Bose (Writer) and Rupi Kaur (Poet)</br> \
-                3. A Conversation between the Visual and Discursive: Tavi Gevinson (Youth Media Icon) and Gui Bonsieppe (Designer and Writer)";
+                * Economics of Misinformation- March 18th, 2019 (11:30-2pm); Tony Hall (BBC Finance Reporter) and Giorgia Lupi (Data Visualiser and Design Researcher)</br> \
+                * Anonimity and Gender Erasure- May 20th, 2019 (6-8pm); Durga Chew Bose (Writer) and Rupi Kaur (Poet)</br> \
+                * A Conversation between the Visual and Discursive- June 2nd, 2019 (6-8pm); Tavi Gevinson (Youth Media Icon) and Gui Bonsieppe (Designer and Writer)";
             break;
     }
     
